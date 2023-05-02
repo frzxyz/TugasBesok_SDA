0065b72bfcac4a5958f6b93b8685b874b8fd7fcb
frzxyz <accsmurfer@gmail.com>
2023-05-02 09:28:56 +0700

    Update Header

--- header.h
+++ header.h
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <time.h>
 #include <stdbool.h>
-// Tim
+//Abstrak data untuk node team.
 typedef struct tim
 {
     char *namaTim, *loseBy;
@@ -11,7 +11,7 @@ typedef struct tim
     int gol, jumlahGol, score;
 } Tim;
 
-// Menyimpan grup
+//Abstrak data untuk grup.
 typedef struct group
 {
     char *namaGroup;
@@ -45,4 +45,76 @@ void displayGroup(Group **group, int numsGroup);
 Modul untuk menampilkan group beserta team-team dalam grup tersebut.
 IS: Grup dan team-teamnya belum dapat ditampilkan.
 FS: Grup dan team-teamnya sudah dapat ditampilkan.
+*/
+void sortTeamByScore(Group **group);
+/*
+Modul untuk mengurutkan tim berdasarkan skor tertinggi ke terendah menggunakan metode bubble sort
+IS: Score setiap team belum terurut dari tertinggi ke terendah.
+FS: Score setiap team sudah terurut dari tertinggi ke terendah.
+*/
+void sortTeamByGoal(Group **group);
+/*
+Modul untuk mengurutkan tim berdasarkan gol tertinggi ke gol terendah jika skor sama
+IS: Score sama dan jumlah gol belum terurut dari tertinggi ke terendah.
+FS: Score sama dan jumlah gol telah terurut dari tertinggi ke terendah.
+*/
+Tim *searchTeam(Group **group, char *key);
+/*
+Fungsi untuk mencari nama dari tim dan akan mengembalikan nama tim tersebut jika ketemu
+IS: Nama tim untuk dicari diketahui dan belum ketemu.
+FS: Nama tim diketahui dan sudah ketemu.
+*/
+void resultmatch(char *tim1, char *tim2);
+/*
+Modul untuk mengadakan pertandingan antara dua tim dengan score menggunakan fungsi random
+IS: Menang,kalah,score,jumlah gol kedua tim belum diketahui.
+FS: Menang,kalah,score,jumlah gol kedua tim sudah diketahui.
+*/
+void match();
+/*
+Modul untuk menyimpan setiap tim yang akan bertanding dengan memanggil prosedur resultmatch
+IS: Tim-Tim dari setiap grup belum bertanding.
+FS: Tim-Tim dari setiap grup telah bertanding.
+*/
+Tim *Penalty(char *Team1, char *Team2);
+/*
+Fungsi untuk menentukan hasil pinalti antara kedua tim.
+IS: Hasil pinalti belum diketahui.
+FS: Hasil pinalti telah diketahui.
+*/
+void sortTeamByHeadToHead(Group **group);
+/*
+Modul untuk mengurutkan team berdasarkan head to head.
+IS: Tim dengan Skor yang sama di grup masih belum bisa ditentukan pemenangnya
+FS: Tim denagn skor yang sama di grup sudah bisa ditentukan pemenangnya
+*/
+Tim *createTree();
+/*
+Fungsi untuk membuat tree dan memanggil modul movetoparent.
+IS: Tree belum terbuat.
+FS: Tree sudah terbuat
+*/
+Tim *winnerMoveToParent(Tim *leftChild, Tim *rightChild);
+/*
+Fungsi untuk menaikan tim yang menang ke parent tree dari child sebelumnya.
+IS: Tim yang menang belum naik ke parent.
+FS: Tim yang menang telah naik ke parent.
+*/
+void MatchTree(Tim *left, Tim *right);
+/*
+Modul untuk melakukan dan menentukan hasil pertandingan ketika berada di bracket (tree)
+IS: Hasil pertandingan belum diketahui.
+FS: Hasil pertandingan telah diketahui.
+*/
+void clearTeam(Group **group);
+/*
+Modul untuk menghapus seluruh tim.
+IS: Seluruh tim belum reset setiap variable nodenya.
+FS: Seluruh tim telah reset setiap variable nodenya.
+*/
+void PrintTree(Tim *root);
+/*
+Modul untuk menampilkan tree.
+IS: Tree dan namatim belum tampil.
+FS: Tree dan namatim sudah tampil.
 */
